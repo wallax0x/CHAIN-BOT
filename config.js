846c0f9d55deac2c2ffdce0fe76a358a70e999f5
Maurício <75014760+wallax0x@users.noreply.github.com>
2025-07-30 20:12:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ module.exports = {
   PREFIX: '.',
   API_KEY: '',
   OWNER_JID: '5555936195112@s.whatsapp.net',
-  GEMINI_API_KEY: 'AIzaSyAGongMnvrUeqaoQ7g_7O6LO2HZVKw3eTA',
-  WEATHER_API_KEY: 'abd1ef45adfd4e629aa161144252206',
-  LOLHUMAN_API_KEY: 'c643ffa17b85ec78a4d89a79',
-  HF_TOKEN: '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',
+  GEMINI_API_KEY: '',
+  WEATHER_API_KEY: '',
+  LOLHUMAN_API_KEY: '',
+  HF_TOKEN: '',
   GEMINI_MODEL_NAME: 'gemini-2.5-flash',
-  TMDB_API_KEY: '4a7b687104994d6e3f40e6ef1a22b3de',
+  TMDB_API_KEY: '',
   // outras configs...
-CONVERT_API_KEY: '9ZL2wENp24BWpwP8hrBDoJoL4TQ2CqMo' // Coloque sua chave aqui
+CONVERT_API_KEY: '' // Coloque sua chave aqui
 
 };
